8e5261b51f3e047d0ea8ed1fe3f623491f91a45f
Denis Korobicyn <deniskorobicyn@users.noreply.github.com>
2020-09-07 11:45:04 +0500

    Add webcam capture and background removal template

--- background-removal/src/lib.rs
+++ background-removal/src/lib.rs
@@ -1,7 +1,15 @@
 use eyre::Result;
-use opencv::core::{Size, Vector, BORDER_DEFAULT};
-use opencv::prelude::Mat;
-use opencv::{imgcodecs, imgproc};
+extern crate opencv;
+use opencv::core::{Vector, Mat};
+use opencv::{
+    core,
+    highgui,
+    prelude::*,
+    video,
+    videoio,
+    imgcodecs
+};
+
 trait BackgroundRemover<Ref> {
     fn remove_background(&self, source: &[Ref]) -> Result<Vec<u8>>
     where
@@ -23,22 +31,12 @@ impl<Ref> BackgroundRemover<Ref> for OpenCv {
     {
         let mut source_matrix = Mat::from_slice_2d(source)?;
         let mut output_matrix = Mat::default()?;
-        imgproc::gaussian_blur(
-            &mut source_matrix,
-            &mut output_matrix,
-            Size {
-                width: 31,
-                height: 31,
-            },
-            -1.,
-            -1.,
-            BORDER_DEFAULT,
-        )?;
 
         let mut output_buffer = Vector::new();
-        let parmeters = Vector::new();
 
-        imgcodecs::imencode(".jpg", &output_matrix, &mut output_buffer, &parmeters)?;
+        // TODO: set things here
+        // No idea how to convert Vec to Mat for filesave
+
         Ok(output_buffer.to_vec())
     }
 }
@@ -46,16 +44,44 @@ impl<Ref> BackgroundRemover<Ref> for OpenCv {
 #[cfg(test)]
 mod tests {
     use crate::{BackgroundRemover, OpenCv};
-    use opencv::core::MatTraitManual;
-    use std::{fs::File, io::Write};
+    extern crate opencv;
+    use opencv::core::{Vector};
+    use opencv::prelude::*;
 
     #[test]
     fn it_works() {
         let opencv = OpenCv::new().unwrap();
-        let input = opencv::imgcodecs::imread("input.jpg", 0).unwrap();
-        let input: Vec<Vec<u8>> = input.to_vec_2d().unwrap();
-        let output = opencv.remove_background(&input).unwrap();
-        let mut output_file = File::create("test.jpg").unwrap();
-        output_file.write_all(&output).unwrap();
+        let mut cam = opencv::videoio::VideoCapture::new(0, opencv::videoio::CAP_ANY).unwrap();  // 0 is the default camera
+        let opened = opencv::videoio::VideoCapture::is_opened(&cam).unwrap();
+        if !opened {
+            panic!("Unable to open default camera!");
+        }
+        let parmeters = Vector::new();
+        let mut i = 0;
+        let number_of_frames = 3;
+        let mut background_extractor = opencv::video::create_background_subtractor_knn(500, 400.0, true).unwrap();
+
+
+        let mut output_buffer = Mat::default().unwrap();
+
+        while i < number_of_frames {
+            let mut frame = Mat::default().unwrap();
+            cam.read(&mut frame);
+            let mut gray = Mat::default().unwrap();
+
+            opencv::imgproc::cvt_color(
+                &frame,
+                &mut gray,
+                opencv::imgproc::COLOR_BGR2GRAY,
+                0
+            ).unwrap();
+
+            background_extractor.apply(&frame, &mut output_buffer, 0.0);
+
+            let filename = format!("/app/temp/no_background{}.png", i);
+            opencv::imgcodecs::imwrite(&filename, &mut output_buffer, &parmeters);
+
+            i += 1;
+        }
     }
 }
